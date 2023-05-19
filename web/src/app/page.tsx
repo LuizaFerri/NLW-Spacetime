032fad6abc755357ac3b2530be20710caf18f6db
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2023-05-18 23:26:19 -0300

    aula 1 - finalizada

---
+++ web/src/app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <main className="grid min-h-screen grid-cols-2">
+      <div className="flex flex-col items-start justify-between px-28 py-16  relative overflow-hidden">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 bg-purple-700 rounded-full blur-full"/>
+      </div>
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{" "}
+            <a href="#" className=" underline hover:text-gray-50">
+              {" "}
+              criar agora!
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
