3935a2e38ac2ac518e0a7a261e791ea7c2535628
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2023-05-26 21:43:42 -0300

    separando os componentes

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,10 @@
+export const CopyRight = () => {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{" "}
+      <a target="_blank" rel="noreferrer" href="https://rocketseat.com.br">
+        Rocketseat{" "}
+      </a>
+    </div>
+  );
+};
